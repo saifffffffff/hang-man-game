d4ec6187f66194e1d3a8af1495393b22ba5663b6
saif.Aldeen <107871928+saifffffffff@users.noreply.github.com>
2024-10-01 16:53:28 -0400

    main.dart

--- main.dart
+++ main.dart
@@ -2,55 +2,81 @@ import "dart:math";
 import "dart:io";
 void main(){
   Game game = Game();
-  List list = ['a','s'];
-  bool n = game.doesNotHaveDashes();
-  print(n);
+  game.start();
 }
 
 
 
 class Game{
-  List words = ["dog", "apple", "bridge"];
+  List words = ["dog", "apple", "bridge","horse", "tree", "monkey","computer", "dart","gym"];
   List hangedMan = ["______",
                     "|  |  ",                    
                     "|  0 ",
                     "| /|\\",
                     "| / \\",
                     "|     "];
+  bool finished = false;
+  
+  
+  
   void start(){
     List word = words[Random().nextInt(words.length)].toString().split('');  
     List dashes = List.generate(word.length,  (index) => "_");
-    print(dashes.join(''));
-    stdout.write("Guess The Letter");
-    String? inputLetter = stdin.readLineSync();
-
-    int i=0;
-    for(String Letter in word){
-      if (Letter == inputLetter){
-        dashes[i] = Letter;
-        if(doesNotHaveDashes(dashes) == true){
-          print("won");
-        } 
-      }
-
-      i+=1;
-    }
-    
-    bool? doesNotHaveDashes(List list){
-      int listLen = list.length; 
-      int j = 0;
-      for(String i in list){
-        if (i != "_"){
-          j+=1;
-          if (j == listLen){
-              return true;    
+    int correctGuesses = 0;
+    int wrongGuesses=0;
+    int copiedCorrectGuesses;  
+   
+    while (!finished){
+      copiedCorrectGuesses = correctGuesses;
+      //print the hanged man
+      print(dashes.join(' '));
+      //get the letter 
+      
+      String guessedLetter = get_value();
+      //check if the letter in the word
+      int i=0;
+      for(String Letter in word){
+        
+        if (Letter == guessedLetter){
+          dashes[i] = Letter;
+          correctGuesses+=1;
+          if(word.join() == dashes.join()){
+            print("won");
+            finished = true;
           } 
         }
+        i+=1;
       }
+      //check if the counter value changed
+      if((correctGuesses - copiedCorrectGuesses) == 0){
+        wrongGuesses += 1;
+        printHangedMan(wrongGuesses);
+        if (wrongGuesses == hangedMan.length){
+          print("You lost");
+          finished = true;
+        }
+      }
+      }
+    }
+      
+  
+  String get_value(){
+    while(true){
+      stdout.write("Guess The Letter:");
+      String guessedLetter = stdin.readLineSync()!;
+      if (guessedLetter.length == 1 && guessedLetter.contains(RegExp("[a-z]"))){
+        return guessedLetter.toLowerCase();
+      }
+      print("Value error\nPlease enter a valid value.");
+    }
+  
+  }
+  
+  void printHangedMan(int To){
+    for(int i = 0; i < To ; i++){
+      print(hangedMan[i]);
     }
 
   }
-
-
 
 }
